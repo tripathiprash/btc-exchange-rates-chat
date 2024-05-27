10ac14468c9c088db17a726bb0c8991e4070d7ff
PRASHASTI <prashastifuture@gmail.com>
2024-05-27 23:01:36 +0530

    added cronjob functionality

--- main.py
+++ main.py
@@ -6,18 +6,17 @@ from examples.coingecko.coingecko_helper import send_request
 load_dotenv()
 
 if __name__ == "__main__":
-    # Fetch data from Rainforest API
-    # try:
-    #     cmd = ["python3", "examples/coingecko/data_ingestion_cron_job.py"]
+    try:
+        cmd = ["python3", "data_ingestion_cron_job.py"]
 
-    #     subprocess.run(cmd, check=True)
-    # except subprocess.CalledProcessError:
-    #     print("Script execution failed.")
-    # except FileNotFoundError:
-    #     print("Python interpreter or the script was not found.")
-    print("script started")
-    send_request()
-    print("called send_request")
+        subprocess.run(cmd, check=True)
+    except subprocess.CalledProcessError:
+        print("Script execution failed.")
+    except FileNotFoundError:
+        print("Python interpreter or the script was not found.")
+    # print("script started")
+    # send_request()
+    # print("called send_request")
 
     # Run Discounts API
     host = os.environ.get("HOST", "0.0.0.0")
